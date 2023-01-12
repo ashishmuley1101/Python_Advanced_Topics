a46e03c72fe84da668394f316a0850407b9590de
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-12 23:40:49 +0530

    Python RegEx match.start(), match.end() and match.span() method.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 
-# Python RegEx match.group() method.
+# Python RegEx match.start(), match.end() and match.span() method.
 
 # A Regular Expression (RegEx) is a sequence of characters that defines a search pattern.
 # Python has a module named "re" to work with RegEx.
-# The group() method returns the part of the string where there is a match.
-# Syntax : match = re.search(pattern, str)
-#          print(match.group())
 
-#------Python RegEx match.group()--------------------
+
+
+#------Python RegEx match.start(), match.end() and match.span()--------------------
 
 
 import re    # importing the re module
@@ -22,10 +21,17 @@ match = re.search(pattern, string)
 
 if match:
   print(match.group())    # return the matching group
+  print(match.start())  # return the starting index
+  print(match.end())  # return the end index
+  print(match.span())  # return the span() function returns a tuple containing start and end index of the matched part.
 else:
   print("pattern not found")
 
-# Output: 102 11
+# Output:
+# 102 11
+# 1
+# 7
+# (1, 7)
 
 
 
