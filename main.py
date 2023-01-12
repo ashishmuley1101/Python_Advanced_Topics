07cea281ff7dbc200d63d6367396454b2485c2fb
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-12 22:53:35 +0530

    Python RegEx re.split() method.

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 
-# Python RegEx re.findall() method.
+# Python RegEx re.split() method.
 
 # A Regular Expression (RegEx) is a sequence of characters that defines a search pattern.
 # Python has a module named "re" to work with RegEx.
-# The re.findall() method returns a list of strings containing all matches.
+# The re.split method splits the string where there is a match
+# and returns a list of strings where the splits have occurred.
+# If the pattern is not found, re.split() returns a list containing the original string.
 
-#------Python RegEx re.findall()--------------------
-
+#------Python RegEx re.split()--------------------
 
 
 import re    # importing the re module
 
 
-string = 'hello 12 hi 891. Howdy 34'
+string = 'Twelve:12 Eighty nine:89.'
 pattern = '\d+'
 
-result = re.findall(pattern, string)
+result = re.split(pattern, string)
 print(result)
 
-# Output: ['12', '891', '34']
+# Output: ['Twelve:', ' Eighty nine:', '.']
+
+
 
