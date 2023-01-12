bf3edebe2ad1e13467462fe345511a1707b5e383
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-12 22:45:56 +0530

    Python RegEx re.findall() method.

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 
-# Python RegEx re.match()
+# Python RegEx re.findall() method.
 
 # A Regular Expression (RegEx) is a sequence of characters that defines a search pattern.
 # Python has a module named "re" to work with RegEx.
+# The re.findall() method returns a list of strings containing all matches.
 
-#------Python RegEx re.match()--------------------
+#------Python RegEx re.findall()--------------------
+
 
 
 import re    # importing the re module
 
-pattern = '^a...s$'
-test_string = 'abyss'
-result = re.match(pattern, test_string)
 
-if result:   # if pattern match to test_string return true or flase
-  print("Search successful.")
-else:
-  print("Search unsuccessful.")
+string = 'hello 12 hi 891. Howdy 34'
+pattern = '\d+'
+
+result = re.findall(pattern, string)
+print(result)
+
+# Output: ['12', '891', '34']
 
