05c26af0122c804954a4661ccd292f6c8e5791ee
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-12 23:01:01 +0530

    Python RegEx re.sub() method.

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 
-# Python RegEx re.split() method.
+# Python RegEx re.sub() method.
 
 # A Regular Expression (RegEx) is a sequence of characters that defines a search pattern.
 # Python has a module named "re" to work with RegEx.
-# The re.split method splits the string where there is a match
-# and returns a list of strings where the splits have occurred.
-# If the pattern is not found, re.split() returns a list containing the original string.
+# The method returns a string where matched occurrences are replaced with the content of replace variable
+# Syntax : re.sub(pattern, replace, string)
 
-#------Python RegEx re.split()--------------------
+#------Python RegEx re.sub()--------------------
 
 
 import re    # importing the re module
 
+# multiline string
+string = 'abc 12\
+de 23 \n f45 6'
 
-string = 'Twelve:12 Eighty nine:89.'
-pattern = '\d+'
+# matches all whitespace characters
+pattern = '\s+'
 
-result = re.split(pattern, string)
-print(result)
+# empty string
+replace = ''
 
-# Output: ['Twelve:', ' Eighty nine:', '.']
+new_string = re.sub(pattern, replace, string)
+print(new_string)
+
+# Output: abc12de23f456
 
 
 
