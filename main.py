8b300e0b957cd538135e4270bca3429286eb89fa
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-12 20:35:01 +0530

    Python Property Setter

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 
-# Python Property Getter.
+# Python Property Setter
 
-# In Python, a getter is a method that is used to retrieve the value of an attribute.
-# It is defined using the @property decorator, which allows the method to be accessed like an attribute.
-# the @property decorator is a built-in function that allows us to define methods that can be accessed like an attribute.
+# In Python, a setter is a method that is used to set the value of an attribute.
+# It is defined using the @<property_name>.setter decorator,
+# <property_name> name of the property that the setter is for.
 
-#------Python Property Getter--------------------
+#------Python Property Setter--------------------
 
-class Student:
-    def __init__(self, first_name):
-        self.first_name = first_name
 
-    # define getter method
-    @property    #  @property decorator to access get_name() like an attribute.
-    def get_name(self):
-        return self.first_name
+class Person:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
 
-# create a new Student object
-student = Student("Bridgelabz")
+    # Define a "name" getter
+    @property
+    def name(self):    # getting the name variable
+        return self._name
 
-# access the first name using data property
-print("Student first_name : ", student.first_name)
+    # Define a "name"  as setter
+    @name.setter       # setting the name variable with value
+    def name(self, value):
+        self._name = value
 
-# access the first name using getter property
-print("Student first_name : ", student.get_name)
+# Create a new Person object
+person = Person("Jack", 30)
+
+# Access and set the "name" attribute using the getter and setter
+print("Old name :", person.name)           # calling the getter method
+person.name = "Bridgelabz"   # calling the setter method passing the value
+print("New name :", person.name)           # calling the getter method
 
 # Output :
-# Student first_name :  Bridgelabz
-# Student first_name :  Bridgelabz
+# Old name : Jack
+# New name : Bridgelabz
